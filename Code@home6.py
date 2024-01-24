dddb9d63adfa6ac965228653b7617e0108bc1d3a
edwardodavinci <Handymoney9626@gmail.com>
2024-01-24 11:51:31 +0000

    Merge remote-tracking branch 'origin/main'

--- Code@home6.py
+++ Code@home6.py
@@ -1,12 +1,12 @@
 # Code@home6_OOP_GUI_Data_Structures
-from guizero import App, Box, PushButton, Text
+from guizero import App, Box, PushButton, Text, TextBox
 
 
 class Stack:
     def __init__(self):
         self.items = []
 
-    def isEmpty(self):
+    def is_empty(self):
         return self.items == []
 
     def push(self, item):
@@ -22,29 +22,36 @@ class Stack:
         return len(self.items)
 
 
-
 def push_item():
+    global stack
     item = input_box.value
     if item:
         stack.push(item)
         input_box.clear()
+    stack_text.value = stack.items
+
 
 def pop_item():
+    global stack, popped_item
     popped_item = stack.pop()
     if popped_item is not None:
-        print("Popped item:", popped_item)  # You can replace this with a GUI display
+        pop_text.value = ("Popped item: ", popped_item)
+    stack_text.value = stack.items
 
 
+popped_item = " "
 app = App("Stack GUI", width=200, height=200)
 stack = Stack()
 
 
-stack_box = Box(app, layout="grid")
-label = Text(stack_box, text="Stack:")
-
-input_box = Text(stack_box, text="")
-push_button = PushButton(stack_box, command=push_item, text="Push")
-
-pop_button = PushButton(stack_box, command=pop_item, text="Pop")
+label = Text(app, text="Stack:")
+input_box = TextBox(app)
+push_button = PushButton(app, command=push_item, text="Push")
+pop_button = PushButton(app, command=pop_item, text="Pop")
+pop_text = Text(app, text=("Popped item:", popped_item))
+result_text = Text(app, text="")
+stack_box = Box(app)
+stack_box.bg = "grey"
+stack_text = Text(stack_box, text=stack.items)
 
 app.display()
