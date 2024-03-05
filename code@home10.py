a27c211d2ee29e5a9ccf6d6f404f370de70d02b2
edwardodavinci <Handymoney9626@gmail.com>
2024-03-05 19:31:29 +0000

    Merge remote-tracking branch 'origin/main'

--- code@home10.py
+++ code@home10.py
@@ -1,10 +1,12 @@
 # Code@home10 Mcnugget numbers
 
 def mcnugget_check(n):
+    # checks if n is negative
     if n < 0:
         return False
     if n == 0:
         return True
+    # iterating through all possible combinations of 6, 9 and 20
     for i in range(n // 20 + 1):
         for j in range(n // 9 + 1):
             for k in range(n // 6 + 1):
@@ -12,15 +14,17 @@ def mcnugget_check(n):
                     return True
     return False
 
+
 def find_all_mcnuggets():
-    mcnuggets=[]
-    non_mcnuggets=[]
-    for i in range(1,100):
+    mcnuggets = []
+    non_mcnuggets = []
+    for i in range(1, 100):
         if mcnugget_check(i):
             mcnuggets.append(i)
         else:
             non_mcnuggets.append(i)
     return mcnuggets, non_mcnuggets
+
 
 mcnuggets, non_mcnuggets = find_all_mcnuggets()
 print(f"All the mcnugget numbers are {mcnuggets}")
---
+++ wordleCracker/code@home11.py
@@ -0,0 +1,57 @@
+import string
+from wordle_words import words
+
+
+alphabet = list(string.ascii_lowercase)
+letterCount = {}
+
+for letter in alphabet:
+    letterCount[letter] = 0
+
+letterFrequency = {}
+for letter in alphabet:
+    letterFrequency[letter] = 0
+
+for word in words:
+    letters = []
+    for char in word:
+        letterCount[char] += 1
+
+#  Sorting and ordering letters by Frequency
+for letter in letterFrequency:
+    letterFrequency[letter] = "{:.2f}".format(letterCount[letter] / len(words))
+
+# print(letterFrequency)
+orderedFrequency = sorted(letterFrequency.items(), key=lambda x: x[1], reverse=True)
+# print(orderedFrequency)
+for letter, frequency in orderedFrequency:
+    print(f"{letter}: {frequency}")
+
+
+def wordscore(word, letterFrequency):
+    score = 0
+    duplicateletter = {}
+    for letter in alphabet:
+        duplicateletter[letter] = False
+    for letter in word:
+        if duplicateletter[letter] == False:
+            score += letterFrequency[letter]
+            duplicateletter[letter] = True
+    return score
+
+
+for letter in letterFrequency:
+    letterFrequency[letter] = float(letterFrequency[letter])
+
+wordscores = {}
+for word in words:
+    wordscores[word] = 0
+    wordscores[word] = wordscore(word, letterFrequency)
+
+orderedWords = sorted(wordscores.items(), key=lambda x: x[1], reverse=True)
+# print(wordscores)
+# print(orderedWords)
+num = 1
+for word, score in orderedWords[0:10]:
+    print(f"{num}:{word}:{score}")
+    num += 1
